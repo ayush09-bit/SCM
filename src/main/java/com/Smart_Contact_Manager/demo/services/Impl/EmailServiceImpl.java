f654715d9c820117b11699d90e342e7bfbadc90e
ayush <ayushchaudhary0909@gmail.com>
2026-01-10 11:51:00 +0530

    send grid mail

--- src/main/java/com/Smart_Contact_Manager/demo/services/Impl/EmailServiceImpl.java
+++ src/main/java/com/Smart_Contact_Manager/demo/services/Impl/EmailServiceImpl.java
@@ -1,11 +1,18 @@
 package com.Smart_Contact_Manager.demo.services.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.Smart_Contact_Manager.demo.services.EmailService;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -14,18 +21,32 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender eMailSender;
 
+    @Value("${sendgrid.api.key}")
+    private String sendGridApiKey;
+
+    @Value("${sendgrid.from.email}")
+    private String fromEmail;
+
     @Override
     public void sendEmail(String to, String subject, String body) {
-        
-        SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        message.setFrom("ayushchaudhary0909@gmail.com");
-        eMailSender.send(message);
+        Email from = new Email(fromEmail);
+        Email toEmail = new Email(to);
+        Content emailContent = new Content("text/plain", body);
+        Mail mail = new Mail(from, subject, toEmail, emailContent);
 
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
 
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+
+            sg.api(request);
+        } catch (Exception e) {
+            throw new RuntimeException("Email sending failed", e);
+        }
     }
 
     @Override
